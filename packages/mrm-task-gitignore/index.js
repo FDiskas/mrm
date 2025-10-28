c0f52568c791d98394fdc84877bc1012a9d36658
Vytenis <projektas@gmail.com>
2025-10-28 20:45:40 +0200

    Refactor .gitignore and include bun lock file
    The main idea is to make it easier to include more package managers in the feature.
    Also, we need to be sure that the local `.env` file is ignored - usually it contains secrets

--- packages/mrm-task-gitignore/index.js
+++ packages/mrm-task-gitignore/index.js
@@ -1,45 +1,43 @@
 const fs = require('fs');
 const { lines } = require('mrm-core');
 
+const LOCK_FILES = [
+    'package-lock.json', // npm
+    'yarn.lock',         // yarn
+    'pnpm-lock.yaml',    // pnpm
+	'bun.lock',          // bun
+];
+
 module.exports = function task() {
-	const remove = ['node_modules'];
-	const add = [
-		'node_modules/',
-		'.DS_Store',
-		'Thumbs.db',
-		'.idea/',
-		'.vscode/',
-		'*.sublime-project',
-		'*.sublime-workspace',
-		'*.log',
-	];
+    const remove = ['node_modules'];
+    
+    const add = [
+        'node_modules/',
+        '.DS_Store',
+        'Thumbs.db',
+        '.idea/',
+        '.vscode/',
+        '*.sublime-project',
+        '*.sublime-workspace',
+        '*.log',
+        '.env*',
+    ];
+    
+    // Find the one lock file that exists in the project
+    const existingLockFile = LOCK_FILES.find(file => fs.existsSync(file));
 
-	// If project uses npm, ignore yarn.lock and pnpm-lock.yaml
-	if (fs.existsSync('package-lock.json')) {
-		add.push('yarn.lock');
-		add.push('pnpm-lock.yaml');
-		remove.push('package-lock.json');
-	}
+    if (existingLockFile) {
+        remove.push(existingLockFile);
+		
+        LOCK_FILES.filter(file => file !== existingLockFile)
+                  .forEach(file => add.push(file));
+    }
 
-	// If project uses Yarn, ignore package-lock.json and pnpm-lock.yaml
-	if (fs.existsSync('yarn.lock')) {
-		remove.push('yarn.lock');
-		add.push('package-lock.json');
-		add.push('pnpm-lock.yaml');
-	}
-
-	// If project uses pnpm, ignore package-lock.json and yarn.lock
-	if (fs.existsSync('pnpm-lock.yaml')) {
-		remove.push('pnpm-lock.yaml');
-		add.push('yarn.lock');
-		add.push('package-lock.json');
-	}
-
-	// .gitignore
-	lines('.gitignore')
-		.remove(remove)
-		.add(add)
-		.save();
+    // .gitignore
+    lines('.gitignore')
+        .remove(remove)
+        .add(add)
+        .save();
 };
 
 module.exports.description = 'Adds .gitignore';
